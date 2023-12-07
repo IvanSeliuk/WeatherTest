6b7fff10f8bcfcc99dad752e320359f77c0d203d
Ivan Seliuk <ivan.seliuk12@gmail.com>
2023-12-07 14:26:44 +0300

    Created project

---
+++ WeatherTest/WeatherTest/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  WeatherTest
+//
+//  Created by Иван Селюк on 7.12.23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ WeatherTest/WeatherTest/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WeatherTest
+//
+//  Created by Иван Селюк on 7.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
